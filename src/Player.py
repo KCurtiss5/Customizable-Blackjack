27258319550e03aa42cbe56be6838e4b381857fe
KCurtiss5 <KobyCurtiss5@gmail.com>
2023-03-22 14:28:08 -0600

    dealer can now reveal his first card

--- src/Player.py
+++ src/Player.py
@@ -1,11 +1,13 @@
 from Hand import Hand
 from abc import ABC, abstractmethod
+from helper_functions import get_hand
 
 
-class Person(ABC):    
+class Person(ABC):
     @abstractmethod
     def __init__(self):
         self.hand = Hand(0)
+
 
 class Dealer(Person):
     def __init__(self, H17=False):
@@ -13,11 +15,11 @@ class Dealer(Person):
         if (not isinstance(H17, bool)):
             raise ValueError(
                 f"Error, invalid H17: {H17}.")
-        self.H17 = H17 #if true, we hit on 17. Else we stand.
+        self.H17 = H17  # if true, we hit on 17. Else we stand.
 
     def should_dealer_hit(self):
         score = self.hand.get_score()
         return self.H17 if score == 17 else score < 17
 
     def reveal(self):
-        return f"Dealer has {self.hand.cards[0]}"
+        return f"Dealer has \n{get_hand(self.hand, hidden_card=True, hidden_card_index=1)}"
