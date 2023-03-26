b4d1a2511eb1c14af4cc84421d03a73a3daf302f
KCurtiss5 <KobyCurtiss5@gmail.com>
2023-03-25 20:22:41 -0600

    improved hand

--- src/Hand.py
+++ src/Hand.py
@@ -3,14 +3,17 @@ from helper_functions import get_hand
 
 
 class Hand:
-    def __init__(self, bet: int, card_list=[]) -> None:
+    def __init__(self, bet=0, card_list=[]) -> None:
         if any(not isinstance(card, Card) for card in card_list):
             raise ValueError(f"Error, invalid card_list: {card_list}.")
         self.cards = card_list
         if (not isinstance(bet, int)):
             raise ValueError(
                 f"Error, invalid bet: {bet}.")
-        self.bet = bet
+        self._bet = bet
+
+    def set_bet(self, bet: int):
+        self._bet = bet
 
     def clear_hand(self) -> None:
         self.__init__(0)
